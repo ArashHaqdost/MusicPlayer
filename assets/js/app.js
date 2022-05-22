ea4c811ed13ddfb4967ba1718b0510f1c68afab0
Arash Haqdost <shokrullahhaqdost87@gmail.com>
2022-05-22 11:07:00 +0500

    added the settings

---
+++ assets/js/app.js
@@ -0,0 +1,22 @@
+let nu1=2601;
+
+if(nu1%2==0){
+    alert('this number is even');
+}
+else{
+    alert('this number is odd');
+}
+//if else  statem ment example
+
+let orgin=300;
+
+if(orgin==100){
+    alert('this orgin is '+orgin);
+}else if(orgin==200){
+    alert('this orgin is '+orgin);
+}else if(orgin==300){
+    alert('this orgin is '+ orgin);
+}else{
+    alert('this orgin is not recognize');
+}
+
