4338cd72df131e57261f5cd6b6fa37d27e1b6493
Arash Haqdost <shokrullahhaqdost87@gmail.com>
2022-05-22 11:26:25 +0500

    complete the  switch statment

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,3 +20,36 @@ if(orgin==100){
     alert('this orgin is not recognize');
 }
 
+// the switch case statemetn
+
+let Great='y';
+let result;
+
+switch(Great){
+    case 'A':
+        result=" Great A";
+        break;
+    case 'B':
+        result="Grat B";
+        break;
+    default:
+        result="no result is noww";
+        break;
+}
+alert(result);
+
+//the second  example of switch
+
+let smple='Yes';
+let isit;
+
+switch(smple){
+    case 'Yes':
+        isit +="Yes";
+    case 'No':
+        isit +="No";
+    default:
+        isit +="try to ansower quit";
+}
+
+alert(isit)
