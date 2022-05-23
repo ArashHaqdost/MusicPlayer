6787d46d23a1ce767b801607051be433ef421a48
Arash Haqdost <shokrullahhaqdost87@gmail.com>
2022-05-23 23:57:08 +0500

    added the js code for audio

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,80 +1,17 @@
-let nu1=2601;
+let currentMusic=0;
 
-if(nu1%2==0){
-    alert('this number is even');
-}
-else{
-    alert('this number is odd');
-}
-//if else  statem ment example
+const music=document.querySelector('#audio');
 
-let orgin=300;
-
-if(orgin==100){
-    alert('this orgin is '+orgin);
-}else if(orgin==200){
-    alert('this orgin is '+orgin);
-}else if(orgin==300){
-    alert('this orgin is '+ orgin);
-}else{
-    alert('this orgin is not recognize');
+function  celiceos(farnhit) {
+    return (5/9)*(farnhit-32);
 }
 
-// the switch case statemetn
+document.getElementById('audioo').innerHTML= celiceos;
 
-let Great='y';
-let result;
-
-switch(Great){
-    case 'A':
-        result=" Great A";
-        break;
-    case 'B':
-        result="Grat B";
-        break;
-    default:
-        result="no result is noww";
-        break;
+let car={
+    name:"corolla",
+    color:'black',
+    price:3000,
+    distinct
 }
-alert(result);
-
-//the second  example of switch
-
-let smple='Yes';
-let isit;
-
-switch(smple){
-    case 'Yes':
-        isit +="Yes";
-    case 'No':
-        isit +="No";
-    default:
-        isit +="try to ansower quit";
-}
-alert(isit);
-//start the loops 
-// for loop condition
-
-for (i=0; i<20; i++){
-    document.write(i+"Hello world");
-}
-//start the while loop
-
-let start= 10;
- while(start<=30){
-     document.write('first one');
-     start++;
- }
-
- //js functionss
-
- function person(){
-     alert("Hello world")
- }
- 
-
- function multipli(num){
-     return num*num*num;
- }
-
-document.write(multipli(7));
+alert(car.name+car.color);
