29a44ee2c92b4c331bcdee309f4ac2c259bf3a87
Arash Haqdost <shokrullahhaqdost87@gmail.com>
2022-05-24 23:34:25 +0500

    added new fetures

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,14 +3,13 @@ let currentMusic=0;
 const music=document.querySelector('#audio');
 
 function  celiceos(farnhit) {
-    return (5/9 )*(farnhit-32);
+    return (5/9)*(farnhit-32);
 }
 
 document.getElementById('audioo').innerHTML= celiceos();
 
 let car={
     name:"corolla",
-    color:'black',
     price:3000,
     distinct: function start(){ return "km"},
 }
